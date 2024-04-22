d06be0a7faae325910130093ac89dc136eba2570
Vinicius Dias <diasvinicius272@gmail.com>
2024-04-22 11:33:13 -0300

    fix: profile

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -1,47 +1,21 @@
 import React from "react";
+import { ProfileContainer, ProfileImg, ProfileP } from "./styles";
 
-export const Profile: React.FC = (): any => {
+const Profile: React.FC = (): any => {
   return (
-    <div style={{ flex: 1 }}>
-      <div style={{ textAlign: "center", padding: "24px" }}>
-        <img
-          src="./src/assets/avatar/avatar.png"
-          alt="foto avatar"
-          style={{
-            width: "100px",
-            height: "100px",
-            borderRadius: "50%",
-            objectFit: "cover",
-          }}
-        />
-        <p>@instagram</p>
-      </div>
+    <div
+      style={{
+        flex: 1,
+        textAlign: "center",
+      }}
+    >
+      <ProfileContainer>
+        <ProfileImg src="./src/assets/avatar/avatar.png" />
+      </ProfileContainer>
 
-      <div>
-        <span>Altera o tema</span>
-      </div>
-      <ul>
-        <li>
-          <a href="#">Inscrever-se no NLW</a>
-        </li>
-        <li>
-          <a href="#">Baixar meu e-book</a>
-        </li>
-        <li>
-          <a href="https://github.com/lybiomoraesjr" target="_blank">
-            Ver meu portifólio
-          </a>
-        </li>
-        <li>
-          <a href="https://rocketseat.com.br/explorer" target="_blank">
-            Conheça o explorer
-          </a>
-        </li>
-      </ul>
-
-      <div>
-        <p>redes socias</p>
-      </div>
+      <ProfileP>@instagram</ProfileP>
     </div>
   );
 };
+
+export default Profile;
