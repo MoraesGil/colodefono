1e6b919e41030599bf447d5edd7ccecf764b8789
Lybio Moraes Junior <lybiomoraesjr@gmail.com>
2024-04-24 11:19:36 -0300

    feat: add name to certificates

--- src/screens/CertificatesPage/CertificatePaths.ts
+++ src/screens/CertificatesPage/CertificatePaths.ts
@@ -1,43 +1,54 @@
-export const certificateImages: Record<number, string[]> = {
-  1: [
+export const certificateImages: Record<string, string[]> = {
+  ["Bacharelado em Fonoaudiologia"]: [
     "./src/assets/certificates/20151212/001/frente.jpg",
     "./src/assets/certificates/20151212/001/verso.jpg",
   ],
-  2: [
+  ["Especialização em Adiologia Clínica e Ocupacional"]: [
     "./src/assets/certificates/20171216/001/frente.jpg",
     "./src/assets/certificates/20171216/001/verso.jpg",
   ],
-  3: [
+  ["Aprimoramento em Disfagia"]: [
     "./src/assets/certificates/20181124/001/frente.jpg",
     "./src/assets/certificates/20181124/001/verso.jpg",
   ],
-  4: ["./src/assets/certificates/20200314/001/frente.jpg"],
-  5: [
+  ["Intervenção na Disfagia de Pacientes Adultos Pós-AVC"]: [
+    "./src/assets/certificates/20200314/001/frente.jpg",
+  ],
+  ["Formação Clínica em Disfagia: do Hospital ao Home Care"]: [
     "./src/assets/certificates/20200819/001/frente.jpg",
     "./src/assets/certificates/20200819/001/verso.jpg",
   ],
-  6: [
+  ["Atuação Fonoaudiológica em Pacientes Adultos com Disfagia Neurogênica"]: [
     "./src/assets/certificates/20210805/001/frente.jpg",
     "./src/assets/certificates/20210805/001/verso.jpg",
   ],
-  7: [
-    "./src/assets/certificates/20211028/001/frente.jpg",
-    "./src/assets/certificates/20211028/001/verso.jpg",
-  ],
-  8: [
+  ["Atuação Fonoaudiológica em Disfagia Mecânica e Alterações Craniofaciais Oromiofuncionais"]:
+    [
+      "./src/assets/certificates/20211028/001/frente.jpg",
+      "./src/assets/certificates/20211028/001/verso.jpg",
+    ],
+  ["Atuação Fonoaudiológica Neotal e Pediátrica"]: [
     "./src/assets/certificates/20220218/001/frente.jpg",
     "./src/assets/certificates/20220218/001/verso.jpg",
   ],
-  9: [
+  ["Diagnóstico por Imagem para Avaliação da Deglutição"]: [
     "./src/assets/certificates/20220414/001/frente.jpg",
     "./src/assets/certificates/20220414/001/verso.jpg",
   ],
-  10: [
+  ["Curso Online Aba e Autismo"]: [
     "./src/assets/certificates/20230131/001/frente.jpg",
     "./src/assets/certificates/20230131/001/verso.jpg",
   ],
-  11: ["./src/assets/certificates/20230330/001/frente.jpg"],
-  12: ["./src/assets/certificates/20230405/001/frente.jpg"],
-  13: ["./src/assets/certificates/20230501/001/frente.jpg"],
-  14: ["./src/assets/certificates/20240403/001/verso.jpg"],
+  ["Teste da Linguinha e Aprofundamento em Frênulo Lingual"]: [
+    "./src/assets/certificates/20230330/001/frente.jpg",
+  ],
+  ["Seletividade Alimentar em Crianças e Adolecentes"]: [
+    "./src/assets/certificates/20230405/001/frente.jpg",
+  ],
+  ["Consultoria em Amamentação para Fonoaudiólogos"]: [
+    "./src/assets/certificates/20230501/001/frente.jpg",
+  ],
+  ["MO e Linguagem: Onde os Caminhos se Cruzam?"]: [
+    "./src/assets/certificates/20240403/001/verso.jpg",
+  ],
 };
