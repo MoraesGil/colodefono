4004ed0f01b2b696dbba904ee9be1302520a5556
Vinicius Dias <diasvinicius272@gmail.com>
2024-04-24 14:52:52 -0300

    fix: bug route

--- src/components/CertificateMenu/CertificateMenu.tsx
+++ src/components/CertificateMenu/CertificateMenu.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import React, { useState } from "react";
 import {
   BackButton,
@@ -14,22 +14,22 @@ interface CertificateMenuProps {
 }
 
 const CertificateMenu: React.FC<CertificateMenuProps> = ({ lengthArray }) => {
-  const initialCertificate = 1;
-
-  const [currentCertificate, setCurrentCertificate] =
-    useState(initialCertificate);
+  const [currentCertificate, setCurrentCertificate] = useState(1);
+  const navigate = useNavigate();
 
   const certificateCount = lengthArray;
 
   const handleNextCertificate = () => {
     const next = (currentCertificate % certificateCount) + 1;
     setCurrentCertificate(next);
+    navigate(`/certificate/${next}`);
   };
 
   const handlePreviousCertificate = () => {
     const previous =
       ((currentCertificate - 2 + certificateCount) % certificateCount) + 1;
     setCurrentCertificate(previous);
+    navigate(`/certificate/${previous}`);
   };
 
   return (
@@ -38,21 +38,15 @@ const CertificateMenu: React.FC<CertificateMenuProps> = ({ lengthArray }) => {
         <Logo className="pd10">Colo de Fono</Logo>
 
         <NavList>
-          <Link
-            to={`/certificate/${currentCertificate}`}
-            onClick={handlePreviousCertificate}
-          >
+          <a onClick={handlePreviousCertificate}>
             <FaCircleArrowLeft size={25} />
-          </Link>
+          </a>
 
           <span>Certificado {currentCertificate}</span>
 
-          <Link
-            to={`/certificate/${currentCertificate}`}
-            onClick={handleNextCertificate}
-          >
+          <a onClick={handleNextCertificate}>
             <FaCircleArrowRight size={25} />
-          </Link>
+          </a>
         </NavList>
 
         <BackButton>
