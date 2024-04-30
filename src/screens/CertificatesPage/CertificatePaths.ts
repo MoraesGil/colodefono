1f6124d80a8f19f4adf2869a416f89f2a124217d
Lybio Moraes Junior <lybiomoraesjr@gmail.com>
2024-04-30 10:33:12 -0300

    feat: add baseUrl to CertificatePaths

--- src/screens/CertificatesPage/CertificatePaths.ts
+++ src/screens/CertificatesPage/CertificatePaths.ts
@@ -1,3 +1,5 @@
+import { baseUrl } from "../../App";
+
 type certificateImages = Record<
   string,
   {
@@ -7,116 +9,116 @@ type certificateImages = Record<
 >;
 
 export const certificateImages: certificateImages = {
-  ["Bacharelado em Fonoaudiologia"]: {
+  [`Bacharelado em Fonoaudiologia`]: {
     favorite: true,
 
     imagePaths: [
-      "/certificates/20151212/001/frente.jpg",
-      "/certificates/20151212/001/verso.jpg",
+      `${baseUrl}/certificates/20151212/001/frente.jpg`,
+      `${baseUrl}/certificates/20151212/001/verso.jpg`,
     ],
   },
 
-  ["Especialização em Adiologia Clínica e Ocupacional"]: {
+  [`Especialização em Adiologia Clínica e Ocupacional`]: {
     favorite: true,
 
     imagePaths: [
-      "/certificates/20171216/001/frente.jpg",
-      "/certificates/20171216/001/verso.jpg",
+      `${baseUrl}/certificates/20171216/001/frente.jpg`,
+      `${baseUrl}/certificates/20171216/001/verso.jpg`,
     ],
   },
 
-  ["Aprimoramento em Disfagia"]: {
+  [`Aprimoramento em Disfagia`]: {
     favorite: false,
 
     imagePaths: [
-      "/certificates/20181124/001/frente.jpg",
-      "/certificates/20181124/001/verso.jpg",
+      `${baseUrl}/certificates/20181124/001/frente.jpg`,
+      `${baseUrl}/certificates/20181124/001/verso.jpg`,
     ],
   },
 
-  ["Intervenção na Disfagia de Pacientes Adultos Pós-AVC"]: {
+  [`Intervenção na Disfagia de Pacientes Adultos Pós-AVC`]: {
     favorite: false,
 
-    imagePaths: ["/certificates/20200314/001/frente.jpg"],
+    imagePaths: [`${baseUrl}/certificates/20200314/001/frente.jpg`],
   },
 
-  ["Formação Clínica em Disfagia: do Hospital ao Home Care"]: {
-    favorite: false,
-
-    imagePaths: [
-      "/certificates/20200819/001/frente.jpg",
-      "/certificates/20200819/001/verso.jpg",
-    ],
-  },
-
-  ["Atuação Fonoaudiológica em Pacientes Adultos com Disfagia Neurogênica"]: {
+  [`Formação Clínica em Disfagia: do Hospital ao Home Care`]: {
     favorite: false,
 
     imagePaths: [
-      "/certificates/20210805/001/frente.jpg",
-      "/certificates/20210805/001/verso.jpg",
+      `${baseUrl}/certificates/20200819/001/frente.jpg`,
+      `${baseUrl}/certificates/20200819/001/verso.jpg`,
     ],
   },
 
-  ["Atuação Fonoaudiológica em Disfagia Mecânica e Alterações Craniofaciais Oromiofuncionais"]:
+  [`Atuação Fonoaudiológica em Pacientes Adultos com Disfagia Neurogênica`]: {
+    favorite: false,
+
+    imagePaths: [
+      `${baseUrl}/certificates/20210805/001/frente.jpg`,
+      `${baseUrl}/certificates/20210805/001/verso.jpg`,
+    ],
+  },
+
+  [`Atuação Fonoaudiológica em Disfagia Mecânica e Alterações Craniofaciais Oromiofuncionais`]:
     {
       favorite: false,
 
       imagePaths: [
-        "/certificates/20211028/001/frente.jpg",
-        "/certificates/20211028/001/verso.jpg",
+        `${baseUrl}/certificates/20211028/001/frente.jpg`,
+        `${baseUrl}/certificates/20211028/001/verso.jpg`,
       ],
     },
 
-  ["Atuação Fonoaudiológica Neotal e Pediátrica"]: {
+  [`Atuação Fonoaudiológica Neotal e Pediátrica`]: {
     favorite: true,
 
     imagePaths: [
-      "/certificates/20220218/001/frente.jpg",
-      "/certificates/20220218/001/verso.jpg",
+      `${baseUrl}/certificates/20220218/001/frente.jpg`,
+      `${baseUrl}/certificates/20220218/001/verso.jpg`,
     ],
   },
 
-  ["Diagnóstico por Imagem para Avaliação da Deglutição"]: {
+  [`Diagnóstico por Imagem para Avaliação da Deglutição`]: {
     favorite: false,
 
     imagePaths: [
-      "/certificates/20220414/001/frente.jpg",
-      "/certificates/20220414/001/verso.jpg",
+      `${baseUrl}/certificates/20220414/001/frente.jpg`,
+      `${baseUrl}/certificates/20220414/001/verso.jpg`,
     ],
   },
 
-  ["Curso Online Aba e Autismo"]: {
+  [`Curso Online Aba e Autismo`]: {
     favorite: false,
 
     imagePaths: [
-      "/certificates/20230131/001/frente.jpg",
-      "/certificates/20230131/001/verso.jpg",
+      `${baseUrl}/certificates/20230131/001/frente.jpg`,
+      `${baseUrl}/certificates/20230131/001/verso.jpg`,
     ],
   },
 
-  ["Teste da Linguinha e Aprofundamento em Frênulo Lingual"]: {
+  [`Teste da Linguinha e Aprofundamento em Frênulo Lingual`]: {
     favorite: false,
 
-    imagePaths: ["/certificates/20230330/001/frente.jpg"],
+    imagePaths: [`${baseUrl}/certificates/20230330/001/frente.jpg`],
   },
 
-  ["Seletividade Alimentar em Crianças e Adolecentes"]: {
+  [`Seletividade Alimentar em Crianças e Adolecentes`]: {
     favorite: false,
 
-    imagePaths: ["/certificates/20230405/001/frente.jpg"],
+    imagePaths: [`${baseUrl}/certificates/20230405/001/frente.jpg`],
   },
 
-  ["Consultoria em Amamentação para Fonoaudiólogos"]: {
+  [`Consultoria em Amamentação para Fonoaudiólogos`]: {
     favorite: false,
 
-    imagePaths: ["/certificates/20230501/001/frente.jpg"],
+    imagePaths: [`${baseUrl}/certificates/20230501/001/frente.jpg`],
   },
 
-  ["MO e Linguagem: Onde os Caminhos se Cruzam?"]: {
+  [`MO e Linguagem: Onde os Caminhos se Cruzam?`]: {
     favorite: false,
 
-    imagePaths: ["/certificates/20240403/001/verso.jpg"],
+    imagePaths: [`${baseUrl}/certificates/20240403/001/verso.jpg`],
   },
 };
 
