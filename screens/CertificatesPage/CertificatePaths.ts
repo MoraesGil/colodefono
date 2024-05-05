0be0524a58127a865f3d229eb534e0c4d2cc3dc6
Gilberto Moraes <moraesdev@gmail.com>
2024-05-05 00:45:29 -0300

    Updates

--- screens/CertificatesPage/CertificatePaths.ts
+++ screens/CertificatesPage/CertificatePaths.ts
@@ -11,8 +11,8 @@ export const certificateImages: certificateImages = {
 		favorite: true,
 
 		imagePaths: [
-			`/certificates/2015-12-12/001/frente.jpg`,
-			`/certificates/2015-12-12/001/verso.jpg`,
+			`/assets/certificates/2015-12-12/001/frente.jpg`,
+			`/assets/certificates/2015-12-12/001/verso.jpg`,
 		],
 	},
 
@@ -20,8 +20,8 @@ export const certificateImages: certificateImages = {
 		favorite: true,
 
 		imagePaths: [
-			`/certificates/2017-12-16/001/frente.jpg`,
-			`/certificates/2017-12-16/001/verso.jpg`,
+			`/assets/certificates/2017-12-16/001/frente.jpg`,
+			`/assets/certificates/2017-12-16/001/verso.jpg`,
 		],
 	},
 
@@ -29,23 +29,23 @@ export const certificateImages: certificateImages = {
 		favorite: false,
 
 		imagePaths: [
-			`/certificates/2018-11-24/001/frente.jpg`,
-			`/certificates/2018-11-24/001/verso.jpg`,
+			`/assets/certificates/2018-11-24/001/frente.jpg`,
+			`/assets/certificates/2018-11-24/001/verso.jpg`,
 		],
 	},
 
 	[`Intervenção na Disfagia de Pacientes Adultos Pós-AVC`]: {
 		favorite: false,
 
-		imagePaths: [`/certificates/2020-03-14/001/frente.jpg`],
+		imagePaths: [`/assets/certificates/2020-03-14/001/frente.jpg`],
 	},
 
 	[`Formação Clínica em Disfagia: do Hospital ao Home Care`]: {
 		favorite: false,
 
 		imagePaths: [
-			`/certificates/2020-08-17/001/frente.jpg`,
-			`/certificates/2020-08-17/001/verso.jpg`,
+			`/assets/certificates/2020-08-17/001/frente.jpg`,
+			`/assets/certificates/2020-08-17/001/verso.jpg`,
 		],
 	},
 
@@ -53,8 +53,8 @@ export const certificateImages: certificateImages = {
 		favorite: false,
 
 		imagePaths: [
-			`/certificates/2021-08-15/001/frente.jpg`,
-			`/certificates/2021-08-15/001/verso.jpg`,
+			`/assets/certificates/2021-08-15/001/frente.jpg`,
+			`/assets/certificates/2021-08-15/001/verso.jpg`,
 		],
 	},
 
@@ -63,8 +63,8 @@ export const certificateImages: certificateImages = {
 			favorite: false,
 
 			imagePaths: [
-				`/certificates/2021-10-28/001/frente.jpg`,
-				`/certificates/2021-10-28/001/verso.jpg`,
+				`/assets/certificates/2021-10-28/001/frente.jpg`,
+				`/assets/certificates/2021-10-28/001/verso.jpg`,
 			],
 		},
 
@@ -72,8 +72,8 @@ export const certificateImages: certificateImages = {
 		favorite: false,
 
 		imagePaths: [
-			`/certificates/2022-02-18/001/frente.jpg`,
-			`/certificates/2022-02-18/001/verso.jpg`,
+			`/assets/certificates/2022-02-18/001/frente.jpg`,
+			`/assets/certificates/2022-02-18/001/verso.jpg`,
 		],
 	},
 
@@ -81,8 +81,8 @@ export const certificateImages: certificateImages = {
 		favorite: false,
 
 		imagePaths: [
-			`/certificates/2022-04-14/001/frente.jpg`,
-			`/certificates/2022-04-14/001/verso.jpg`,
+			`/assets/certificates/2022-04-14/001/frente.jpg`,
+			`/assets/certificates/2022-04-14/001/verso.jpg`,
 		],
 	},
 
@@ -90,32 +90,32 @@ export const certificateImages: certificateImages = {
 		favorite: false,
 
 		imagePaths: [
-			`/certificates/2023-01-31/001/frente.jpg`,
-			`/certificates/2023-01-31/001/verso.jpg`,
+			`/assets/certificates/2023-01-31/001/frente.jpg`,
+			`/assets/certificates/2023-01-31/001/verso.jpg`,
 		],
 	},
 
 	[`Teste da Linguinha e Aprofundamento em Frênulo Lingual`]: {
 		favorite: false,
 
-		imagePaths: [`/certificates/2023-03-30/001/frente.jpg`],
+		imagePaths: [`/assets/certificates/2023-03-30/001/frente.jpg`],
 	},
 
 	[`Seletividade Alimentar em Crianças e Adolecentes`]: {
 		favorite: false,
 
-		imagePaths: [`/certificates/2023-04-05/001/frente.jpg`],
+		imagePaths: [`/assets/certificates/2023-04-05/001/frente.jpg`],
 	},
 
 	[`Consultoria em Amamentação para Fonoaudiólogos`]: {
 		favorite: false,
 
-		imagePaths: [`/certificates/2023-05-01/001/frente.jpg`],
+		imagePaths: [`/assets/certificates/2023-05-01/001/frente.jpg`],
 	},
 
 	[`MO e Linguagem: Onde os Caminhos se Cruzam?`]: {
 		favorite: false,
 
-		imagePaths: [`/certificates/2024-04-03/001/frente.jpg`],
+		imagePaths: [`/assets/certificates/2024-04-03/001/frente.jpg`],
 	},
 };
